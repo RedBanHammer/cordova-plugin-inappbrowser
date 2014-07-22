a0cdb4b0f72fdf09644c172890a6503644d1d234
RedBanHammer <redbanhammer@Andrews-MacBook-Pro.local>
2014-07-22 06:59:22 -1000

    Fixed return statement

--- src/android/InAppBrowserBeta.java
+++ src/android/InAppBrowserBeta.java
@@ -527,7 +527,7 @@ public class InAppBrowserBeta extends CordovaPlugin {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             // Inflate the layout for this fragment
-            //return inflater.inflate(R.layout.afragment, container, false);
+            return inflater.inflate(R.layout.afragment, container, false);
         }
     } 
 
