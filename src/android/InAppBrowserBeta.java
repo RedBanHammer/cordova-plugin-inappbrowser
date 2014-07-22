8020414b4312648212553d993084dae88cdfcbb2
RedBanHammer <redbanhammer@Andrews-MacBook-Pro.local>
2014-07-21 21:56:54 -1000

    Testing hide, loadstatus interfaces

--- src/android/InAppBrowserBeta.java
+++ src/android/InAppBrowserBeta.java
@@ -203,6 +203,24 @@ public class InAppBrowserBeta extends CordovaPlugin {
             pluginResult.setKeepCallback(true);
             this.callbackContext.sendPluginResult(pluginResult);
         }
+        else if (action.equals("loadedStatus")) {
+            webView.evaluateJavascript("(function() { return (document.getElementById('shotbowAppPageLoaded')!=null ? document.getElementById('shotbowAppPageLoaded').getAttribute('token') : 'false').toString(); })();", new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String s) {
+                    try {
+                        JSONObject obj = new JSONObject();
+                        obj.put("type", "loadedStatus");
+                        obj.put("loaded", s);
+            
+                        sendUpdate(obj, true);
+                    } catch (JSONException ex) {
+                        Log.d(LOG_TAG, "Should never happen");
+                    }
+                }
+            });
+        } else if (action.equals("hide")) {
+            hideDialog();
+        }
         else {
             return false;
         }
@@ -326,6 +344,18 @@ public class InAppBrowserBeta extends CordovaPlugin {
     }
 
     /**
+     * Hides the dialog but does NOT dismiss it
+     */
+    public void hideDialog() {
+        this.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (dialog != null) dialog.hide();
+            }
+        });
+    }
+
+    /**
      * Closes the dialog
      */
     public void closeDialog() {
@@ -341,11 +371,11 @@ public class InAppBrowserBeta extends CordovaPlugin {
                 childView.setWebViewClient(new WebViewClient() {
                     // NB: wait for about:blank before dismissing
                     public void onPageFinished(WebView view, String url) {
-                if (dialog != null) {
-                    dialog.dismiss();
-                }
-            }
-        });
+                        if (dialog != null) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
                 // NB: From SDK 19: "If you call methods on WebView from any thread 
                 // other than your app's UI thread, it can cause unexpected results."
                 // http://developer.android.com/guide/webapps/migrating.html#Threads
@@ -816,5 +846,9 @@ public class InAppBrowserBeta extends CordovaPlugin {
             }
         	
         }
+
+
+        // Loaded status event to make up for lack of js callbacks
+
     }
 }
