d3f1e9d008f4798aa985dab7490d46b052f438ce
RedBanHammer <redbanhammer@Andrews-MacBook-Pro.local>
2014-07-22 07:10:08 -1000

    Nope

--- src/android/InAppBrowserBeta.java
+++ src/android/InAppBrowserBeta.java
@@ -525,11 +525,11 @@ public class InAppBrowserBeta extends CordovaPlugin {
     }
 
     class AFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //@Override
+        //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.afragment, container, false);
-        }
+         //   return inflater.inflate(R.layout.afragment, container, false);
+        //}
     } 
 
     /**
