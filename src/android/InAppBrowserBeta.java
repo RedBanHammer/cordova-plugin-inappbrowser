da931f67d4c1adebff735f487d5654c8dc58872c
RedBanHammer <redbanhammer@Andrews-MacBook-Pro.local>
2014-07-22 18:15:43 -1000

    Crashing issues

--- src/android/InAppBrowserBeta.java
+++ src/android/InAppBrowserBeta.java
@@ -410,7 +410,7 @@ public class InAppBrowserBeta extends CordovaPlugin {
         this.cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                childView.setWebViewClient(new WebViewClient() {
+                /*childView.setWebViewClient(new WebViewClient() {
                     // NB: wait for about:blank before dismissing
                     public void onPageFinished(WebView view, String url) {
                         if (dialog != null) {
@@ -426,7 +426,9 @@ public class InAppBrowserBeta extends CordovaPlugin {
                 // NB: From SDK 19: "If you call methods on WebView from any thread 
                 // other than your app's UI thread, it can cause unexpected results."
                 // http://developer.android.com/guide/webapps/migrating.html#Threads
-                childView.loadUrl("about:blank");
+                childView.loadUrl("about:blank");*/
+                
+                dialog.hide();
             }
         });
 
